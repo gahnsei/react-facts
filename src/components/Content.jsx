f1080e817b91916c67a99306e6d7efd364b71ad4
Sei Gahn <gahnsei@gmail.com>
2022-04-05 09:42:45 -0500

    first push

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+const Content = () => {
+  return (
+    <main className="main-content-container">
+      <h1 className="main-content-header">Fun facts about React</h1>
+      <ul className="main-content-list">
+        <li>Was first released in 2013 </li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained bt Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+};
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <nav className="nav">
+        <img
+          src="https://cubettech.com/wp-content/uploads/2018/09/1280px-React-icon.svg_.png"
+          alt="React"
+          className="nav-logo"
+        />
+        <span className="nav-logo-text">ReactFacts</span>
+        <span className="nav-project-name">React Course - Project 1</span>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
